69775d216fa1b8196c1477a86db9a3dd5363aff7
Caner Ecertemel <Caner.Ecertemel@halkbank.com.tr>
2020-05-20 11:32:42 +0300

    safds

---
+++ src/main/java/test/testv2/App.java
@@ -0,0 +1,13 @@
+package test.testv2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
